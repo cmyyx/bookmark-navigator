6beaa0ff3fe0afb08a4e2ac5ee992a74d10e9319
cmyyx <140233372+cmyyx@users.noreply.github.com>
2025-07-19 18:48:28 +0800

    去除遗留无用功能

--- src/script.js
+++ src/script.js
@@ -27,7 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
             const firstFolder = findFirstFolderWithBookmarks(bookmarksData);
             if (firstFolder) {
                 renderBookmarks(firstFolder.bookmarks);
-                updateOnlineStatus();
                 // TODO: Highlight the first folder
             }
         }
@@ -313,50 +312,14 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     init();
-});
 
-// --- Offline Mode Logic ---
-const placeholderSrc = 'assets/placeholder_icon.svg';
-
-function updateOnlineStatus() {
-  const toastNotification = document.getElementById('toast-notification');
-  const isOnline = navigator.onLine;
-  
-  if (isOnline) {
-    toastNotification.classList.remove('show');
-    // 等待动画结束（0.5s）后，再添加 hidden 类，以便 display: none
-    setTimeout(() => {
-      if (navigator.onLine) { // 再次检查，确保在延迟期间状态没有再次变为离线
-        toastNotification.classList.add('hidden');
-      }
-    }, 500);
-
-    // 移除所有占位符的特殊样式
-    document.querySelectorAll(`img.is-placeholder-offline`).forEach(img => {
-      img.classList.remove('is-placeholder-offline');
+    window.addEventListener('load', () => {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/sw.js').then(registration => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            }).catch(error => {
+                console.error('Service Worker registration failed:', error);
+            });
+        }
     });
-  } else {
-    toastNotification.classList.remove('hidden'); // 移除 display: none
-    // 使用一个微小的延迟来确保浏览器渲染了 display 的变化，从而使动画能够触发
-    setTimeout(() => {
-        toastNotification.classList.add('show');
-    }, 10);
-
-    // 为所有占位符图标添加特殊样式
-    document.querySelectorAll(`img[src$="${placeholderSrc}"]`).forEach(img => {
-      img.classList.add('is-placeholder-offline');
-    });
-  }
-}
-
-window.addEventListener('online', updateOnlineStatus);
-window.addEventListener('offline', updateOnlineStatus);
-window.addEventListener('load', () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    }).catch(error => {
-      console.error('Service Worker registration failed:', error);
-    });
-  }
 });
